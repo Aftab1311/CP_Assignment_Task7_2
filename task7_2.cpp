eb63a9505f29a1d5bab6dcb9f1cc0ecb72ab5db6
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-23 19:42:31 +0530

    Add files via upload

---
+++ task7_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b) {
+   if (a == 0)
+      return b;
+   return gcd(b%a, a);
+}
+int ArrayGcd(int A[], int n) {
+   int res = A[0];
+   for(int i = 1; i < n; i++) {
+      res = gcd(A[i], res);
+   }
+   return res;
+}
+main() {
+   int A[] = {4, 8, 16, 24};
+   int n = sizeof(A)/sizeof(A[0]);
+   cout << "GCD of array elements: " << ArrayGcd(A, n);
+}
